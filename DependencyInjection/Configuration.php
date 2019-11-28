4f42d42f3cf8f1b27077b07b3297f03fc7fe2889
Nenad <nenad.stefanovic@acumenics.com>
2019-11-28 16:33:43 +0100

    Fix for Tree Builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -29,10 +29,19 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('fos_oauth_server');
 
-        /** @var ArrayNodeDefinition $rootNode */
-        $rootNode = $treeBuilder->root('fos_oauth_server');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('fos_oauth_server');
+        }
+
+//        $treeBuilder = new TreeBuilder();
+//
+//        /** @var ArrayNodeDefinition $rootNode */
+//        $rootNode = $treeBuilder->root('fos_oauth_server');
 
         $supportedDrivers = ['orm', 'mongodb', 'propel', 'custom'];
 
